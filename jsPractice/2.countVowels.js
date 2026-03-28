aaa5a1fc00918f305609a6e1248758440a3d2ca7
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2026-03-28 18:52:38 +0000

    add more js practice

---
+++ jsPractice/2.countVowels.js
@@ -0,0 +1,38 @@
+/*
+2️⃣ Count Vowels
+
+Write a function that takes a string and returns the number of vowels (a, e, i, o, u) in it. Ignore capitalization.
+
+Example:
+
+"Hello World" → 3
+*/
+
+function vowelCounter(wordStr) {
+
+    // makes string lowercase
+    const lowerCaseStr = wordStr.toLowerCase();
+
+    // for containing total num of vowels
+    let totalNumOfVowels = 0;
+
+    // iterate (1 by 1) through word string
+    for (let i = 0; i < lowerCaseStr.length; i++) {
+
+        // check if individual char is vowel
+        if (lowerCaseStr[i] == "a" || lowerCaseStr[i] == "e" || lowerCaseStr[i] == "i" || lowerCaseStr[i] == "o" || lowerCaseStr[i] == "u") {
+            totalNumOfVowels++;
+        }
+    }
+
+    return totalNumOfVowels;
+}
+
+const greeting = "HEllo WOrld";
+console.log(vowelCounter(greeting));
+
+const person = "BOb";
+console.log(vowelCounter(person));
+
+const catchPhrase = "When you make a mistAke, give Up!";
+console.log(vowelCounter(catchPhrase));
---
+++ jsPractice/3.reverseArr.js
@@ -0,0 +1,10 @@
+/*
+3️⃣ Reverse an Array
+
+Write a function that takes an array and returns a new array with elements in reverse order.
+
+Example:
+
+[1,2,3] → [3,2,1]
+*/
+
