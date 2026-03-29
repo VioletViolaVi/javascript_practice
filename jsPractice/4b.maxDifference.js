e22fce89b4d4456d1843f890fd4bcbe81b62f1a9
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2026-03-29 00:09:08 +0000

    add more js practice

--- jsPractice/4b.maxDifference.js
+++ jsPractice/4b.maxDifference.js
@@ -8,3 +8,30 @@ Example:
 [3,10,6] → 10 - 3 = 7
 */
 
+function valDiff(numsArr) {
+
+    // for both largest and smallest numbers
+    let maxNum = -Infinity;
+    let minNum = Infinity;
+    
+    for (let i = 0; i < numsArr.length; i++) {
+        
+        // find largest num
+        if (numsArr[i] > maxNum) {
+            maxNum = numsArr[i];
+        }
+
+        // find smallest num
+        if (numsArr[i] < minNum) {
+            minNum = numsArr[i];
+        }        
+    }
+
+    // subtract numbers to get difference
+    let maxDiff = maxNum - minNum;
+
+    // return difference
+    return maxDiff;
+}
+
+console.log(valDiff([3,10,6]));
---
+++ jsPractice/5.runningTotals.js
@@ -0,0 +1,13 @@
+/*
+5️⃣ Running Total
+
+Write a function that takes an array and returns an array of running totals.
+
+Example:
+
+[1,2,3] → [1, 3, 6]  // 1, 1+2=3, 1+2+3=6
+*/
+
+function runningTotal(numsArr) {
+    
+}
